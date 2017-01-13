f5e5c1ad664a3106e018ff4651c4b490c818ecff
taita2015 <mmix.xia@gmail.com>
2017-01-13 23:41:22 +0800

    add bootstrap

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-chrome.bookmarks.getTree(function(tree){
+chrome.bookmarks.getTree(function(tree) {
 	var Trees = tree[0].children;
 	for (var i = 0; i < Trees.length; i++) {
 		console.log(Trees[i].title);
 	}
 });
 
-chrome.bookmarks.onCreated.addListener(function(callback){
+chrome.bookmarks.onCreated.addListener(function(callback) {
 	console.log(callback);
 });
 
@@ -22,22 +22,20 @@ chrome.bookmarks.onCreated.addListener(function(callback){
 //     console.log("saved");
 //   });
 
- // chrome.storage.sync.get({
- //    favoriteColor: 'red',
- //    likesColor: true
- //  }, function(items) {
- //  	console.log(items);
- //  });
+// chrome.storage.sync.get({
+//    favoriteColor: 'red',
+//    likesColor: true
+//  }, function(items) {
+//  	console.log(items);
+//  });
 
- // chrome.webRequest.onResponseStarted.addListener(function(requestId,...){
- // 	console.log(requestId);
- // });
-
-
+// chrome.webRequest.onResponseStarted.addListener(function(requestId,...){
+// 	console.log(requestId);
+// });
 
 
 
- /**allow to access
+/**allow to access
 	Object
 Event
 :
@@ -94,18 +92,20 @@ __proto__
 :
 Object
  */
-var changePath = ["/image/mushroom_green.png","/image/mushroom_red.png"];
+var changePath = ["/image/mushroom_green.png", "/image/mushroom_red.png"];
 
 var current_loop = 0;
 var current_count = 0;
-setInterval(function(){
+setInterval(function() {
 	chrome.browserAction.setIcon({
-    path: changePath[current_loop++],
-    // tabId: sender.tab.id
-});
-    current_loop = current_loop%changePath.length;
-	chrome.browserAction.setBadgeText({text: ""+current_count++});
-},500);
+		path: changePath[current_loop++],
+		// tabId: sender.tab.id
+	});
+	current_loop = current_loop % changePath.length;
+	chrome.browserAction.setBadgeText({
+		text: "" + current_count++
+	});
+}, 500);
 
 // console.log(chrome.browserAction);
 
@@ -125,11 +125,12 @@ setInterval(function(){
 // );
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    console.log(details);
-    return { requestHeaders: details.requestHeaders };
-  },
-  {urls: ['<all_urls>']},
-  ['requestHeaders']
+	function(details) {
+		console.log(details);
+		return {
+			requestHeaders: details.requestHeaders
+		};
+	}, {
+		urls: ['<all_urls>']
+	}, ['requestHeaders']
 );
-
--- content_script.js
+++ content_script.js
@@ -5,26 +5,23 @@
 // document.write('');
 
 var cssId = "coco_css";
-if (!document.getElementById(cssId))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    var url   = chrome.extension.getURL("css/content_css.css");
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = url;
-    link.media = 'all';
-    head.appendChild(link);
+if (!document.getElementById(cssId)) {
+	var head = document.getElementsByTagName('head')[0];
+	var link = document.createElement('link');
+	var url = chrome.extension.getURL("css/content_css.css");
+	link.id = cssId;
+	link.rel = 'stylesheet';
+	link.type = 'text/css';
+	link.href = url;
+	link.media = 'all';
+	head.appendChild(link);
 }
 
 var divId = "coco_div";
-if (!document.getElementById(divId))
-{
-    var html  = document.getElementsByTagName('html')[0];
-    console.log(html);
-    var div  = document.createElement('div');
-    div.id   = divId;
-    html.appendChild(div);
+if (!document.getElementById(divId)) {
+	var html = document.getElementsByTagName('html')[0];
+	console.log(html);
+	var div = document.createElement('div');
+	div.id = divId;
+	html.appendChild(div);
 }
-
