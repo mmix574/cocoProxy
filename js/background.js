0455262f45b1ea99398182f23d35d468bd56050c
taita2015 <mmix.xia@gmail.com>
2017-01-17 17:24:38 +0800

    0117

--- js/background.js
+++ js/background.js
@@ -1,24 +1,37 @@
 // make it easy to debug
 var background_log = console.log;
 console.log = function(message){
-		background_log("["+new Date().toLocaleTimeString()+"  background.js] "+message);
+		if(message instanceof Object){
+			background_log("["+new Date().toLocaleTimeString()+"  background.js] ");
+			background_log(message);
+		}else {
+			background_log("["+new Date().toLocaleTimeString()+"  background.js] "+message);
+		}
 }
 
 //--------------start coding ---------------------//
 
 var background = {
-	proxy:{},
-	log:{},
+	proxy:"system",//default_value
+	log:[],
 	init:function(){
 		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-			
-			console.log("requestcall",request);
+			if(request.fn in background){
+					background[request.fn](request,sender,sendResponse);
+			}else{
+				console.log("popup request for a function name "+ request.fn+"which is not defined in background.js");
+			}
 		});
+	},
+	setProxy:function(request,sender,sendResponse){
+		this.proxy = requset.proxy;
+	},
+	getProxy:function(request,sender,sendResponse){
+		sendResponse({proxy:this.proxy});
+	},
+	addLog:function(request,sender,sendResponse){
+		
 	}
 };
 
-
 background.init();
-
-
-/*response all the message*/
--- popup.js
+++ popup.js
@@ -1,20 +1,27 @@
 //make it easy to debug
 var background_console =  chrome.extension.getBackgroundPage().console;
 console.log = function (message){
+  if(message instanceof Object){
+    background_console.log("["+new Date().toLocaleTimeString()+"  popup.js] ");
+    var output_message = "";
+    for(var key in message){
+      output_message+=key+":";
+      output_message+=message[key]+" ";
+    }
+    background_console.log(output_message);
+  }else{
     background_console.log("["+new Date().toLocaleTimeString()+"  popup.js] "+message);
+  }
 }
 
 //--------------start coding ---------------------//
 
 var app = {
-
   pageBehavior:{
     init: function() {
       this.domActionBind();
     },
-
-
-    //Behavior
+    //Only for dom Behavior operation
     setBtnActive: function(witch) {
       var btns = document.querySelectorAll(".btn");
       for (var i = 0; i < btns.length; i++) {
@@ -23,27 +30,42 @@ var app = {
       var btn = document.querySelector("#" + witch);
       btn.classList.add("btn_selected");
     },
-    // log: function(message) {
-    //   var div_console = document.querySelector("#console");
-    //   var output_message = "[" + new Date().toLocaleTimeString() + "] " + message;
-    //   div_console.innerHTML += output_message + "<br/>";
-    // },
-
-    //Dom Button Click Action
+    addLogToPopupDiv: function(message) {
+      // message.time ,message.content
+      console.log(message.time,message.content);
+    },
     domActionBind: function() {
-      var that = this;
       var btns = document.querySelectorAll(".btn");
       for (var i = 0; i < btns.length; i++) {
         btns[i].addEventListener("click", function() {
-          that.setBtnActive(this.id);
+          app.pageBehavior.setBtnActive(this.id);
         });
       }
     }
   },
+  // All action in pageController
   pageController:{
+    init:function(){
+      chrome.runtime.sendMessage({fn:"getProxy"},function(response){
+          app.pageBehavior.setBtnActive(response.proxy);
+      });
+    },
     takeControl:function(){
+        app.pageBehavior.init();
+        app.pageController.init();
+    },
+    bindBackgroundMessageAction:function(){
+        chrome.runtime.onMessage.addListener(function(request.sender,sendResponse){
+            if(request.fn in app.router){
+              app.router[request.fn](request.sender,sendResponse);
+            }else{
+                console.log("");
+            }
+        });
+    },
+  },
+  router:{
 
-    }
   },
   start:function(){
     this.pageController.takeControl();
