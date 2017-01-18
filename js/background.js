fa6117b336e5e5e7f7f957a352a541d0fb71ea5b
taita2015 <mmix.xia@gmail.com>
2017-01-18 11:30:38 +0800

    0118

--- js/background.js
+++ js/background.js
@@ -1,37 +1,86 @@
 // make it easy to debug
-var background_log = console.log;
-console.log = function(message){
-		if(message instanceof Object){
-			background_log("["+new Date().toLocaleTimeString()+"  background.js] ");
-			background_log(message);
-		}else {
-			background_log("["+new Date().toLocaleTimeString()+"  background.js] "+message);
-		}
+var old_console = {};
+old_console.log = console.log;
+console.log = function(msg) {
+	old_console.log("[" + new Date().toLocaleTimeString() + "  background.js] ");
+	old_console.log(msg);
 }
 
 //--------------start coding ---------------------//
 
+
+//share code area
+var share = this;
+share.storage = {};
+share.has = function(obj){
+	if(obj in share.storage){
+		return true;
+	}else{
+		return false;
+	}
+}
+share.put = function(key,value){
+	if(key in share.storage){return false;}
+	else{
+		share.storage.key = value;
+	}
+}
+share.get = function(key){
+	if(key in share.storage){
+		return share.storage.key;
+	}else{
+		return null;
+	}
+}
+
+
 var background = {
-	proxy:"system",//default_value
-	log:[],
-	init:function(){
+	proxy: "system", //default_value
+	log: [],
+	init: function() {
 		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-			if(request.fn in background){
-					background[request.fn](request,sender,sendResponse);
-			}else{
-				console.log("popup request for a function name "+ request.fn+"which is not defined in background.js");
+			if (request.fn in background) {
+				background[request.fn](request, sender, sendResponse);
+			} else {
+				console.log("popup request for a function name " + request.fn + "which is not defined in background.js");
 			}
 		});
 	},
-	setProxy:function(request,sender,sendResponse){
-		this.proxy = requset.proxy;
+	setProxy: function(request, sender, sendResponse) {
+		this.proxy = request.proxy;
 	},
-	getProxy:function(request,sender,sendResponse){
-		sendResponse({proxy:this.proxy});
+	getProxy: function(request, sender, sendResponse) {
+		sendResponse({
+			proxy: this.proxy
+		});
 	},
-	addLog:function(request,sender,sendResponse){
-		
+	addLog: function(request, sender, sendResponse) {
+
+	},
+	test: function() {
+		setInterval(function(){
+			if(share.has("key")){
+				console.log(share.get("key"));
+			}
+		},1000);
+	}
+};
+
+var front = {
+	log: function(msg) {
+		var mssg = {};
+		mssg.time = "20000";
+		mssg.content = "this is the content of msg.content";
+		chrome.runtime.sendMessage({
+			fn:"log",
+			mssg:mssg
+		}, function(response) {
+			console.log(response);
+		});
 	}
 };
 
 background.init();
+background.test();
+
+console.log();
--- popup.js
+++ popup.js
@@ -1,75 +1,103 @@
 //make it easy to debug
-var background_console =  chrome.extension.getBackgroundPage().console;
-console.log = function (message){
-  if(message instanceof Object){
-    background_console.log("["+new Date().toLocaleTimeString()+"  popup.js] ");
-    var output_message = "";
-    for(var key in message){
-      output_message+=key+":";
-      output_message+=message[key]+" ";
-    }
-    background_console.log(output_message);
-  }else{
-    background_console.log("["+new Date().toLocaleTimeString()+"  popup.js] "+message);
-  }
+var old_console = chrome.extension.getBackgroundPage().old_console;
+console.log = function(msg) {
+    old_console.log("[" + new Date().toLocaleTimeString() + "  popup.js] ");
+    old_console.log(msg);
 }
+
+// share code area
+var share = chrome.extension.getBackgroundPage();
+
+
 
 //--------------start coding ---------------------//
 
 var app = {
-  pageBehavior:{
-    init: function() {
-      this.domActionBind();
-    },
-    //Only for dom Behavior operation
-    setBtnActive: function(witch) {
-      var btns = document.querySelectorAll(".btn");
-      for (var i = 0; i < btns.length; i++) {
-        btns[i].classList.remove("btn_selected");
-      }
-      var btn = document.querySelector("#" + witch);
-      btn.classList.add("btn_selected");
-    },
-    addLogToPopupDiv: function(message) {
-      // message.time ,message.content
-      console.log(message.time,message.content);
-    },
-    domActionBind: function() {
-      var btns = document.querySelectorAll(".btn");
-      for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function() {
-          app.pageBehavior.setBtnActive(this.id);
-        });
-      }
-    }
-  },
-  // All action in pageController
-  pageController:{
-    init:function(){
-      chrome.runtime.sendMessage({fn:"getProxy"},function(response){
-          app.pageBehavior.setBtnActive(response.proxy);
-      });
-    },
-    takeControl:function(){
-        app.pageBehavior.init();
-        app.pageController.init();
-    },
-    bindBackgroundMessageAction:function(){
-        chrome.runtime.onMessage.addListener(function(request.sender,sendResponse){
-            if(request.fn in app.router){
-              app.router[request.fn](request.sender,sendResponse);
-            }else{
-                console.log("");
+    view: {
+        init: function() {
+            this.domActionBind();
+        },
+        //Only for dom Behavior operation
+        setBtnActive: function(witch) {
+            var btns = document.querySelectorAll(".btn");
+            for (var i = 0; i < btns.length; i++) {
+                btns[i].classList.remove("btn_selected");
             }
-        });
-    },
-  },
-  router:{
+            var btn = document.querySelector("#" + witch);
+            btn.classList.add("btn_selected");
+        },
+        addLogToPopupDiv: function(message) {
+            // message.time ,message.content
+            var textarea = document.querySelector("#console");
+            console.log(textarea);
+        },
+        domActionBind: function() {
+            var btns = document.querySelectorAll(".btn");
+            for (var i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click", function() {
+                    app.view.setBtnActive(this.id);
+                    if(app.view.fn.btndown instanceof Function){
+                        app.view.fn.btndown(this.id);
+                    }
+                });
 
-  },
-  start:function(){
-    this.pageController.takeControl();
-  }
+            }
+        },
+        //call when button is push down
+        fn:{
+            btndown:{}
+        },
+    },
+    // All action in controller
+    controller: {
+        init: function() {
+            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+                if (request.fn in app.router) {
+                    sendResponse({
+                        msg: "ok"
+                    });
+                    app.router[request.fn](request, sender, sendResponse);
+                } else {
+                    sendResponse({
+                        msg: "no matchs method in router"
+                    });
+                }
+            });
+            chrome.runtime.sendMessage({
+                fn: "getProxy"
+            }, function(response) {
+                app.view.setBtnActive(response.proxy);
+            });
+
+            //
+            app.view.fn.btndown = function(id){
+                chrome.runtime.sendMessage({fn:"setProxy",proxy:id});
+            }
+
+        },
+        takeControl: function() {
+            app.view.init();
+            app.controller.init();
+        },
+    },
+    router: {
+        log: function(request, sender, sendResponse) {
+            console.log(request);
+        }
+    },
+    start: function() {
+        this.controller.takeControl();
+    },
+    test: function() {
+        app.view.addLogToPopupDiv({
+            time: "22",
+            content: "hsihsi"
+        });
+    }
 };
 
+
+
+
 app.start();
+app.test();
